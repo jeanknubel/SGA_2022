e09661981d8a3198e9ba26acadda04e97b72b9dd
jeanknubel <jeanknubel@gmail.com>
2022-08-15 16:59:27 +0200

    ajout du projet unity

---
+++ SGA/Assets/script/character_manager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//gère les déplacements des 2 joueurs
+public class character_manager : MonoBehaviour
+{
+   
+    
+    private GameObject player1, player2;
+
+    void Update()
+    {
+        
+    }
+}
