564a6a42aa0e583941b44ca80b010f7ed86f6c4b
spritetheo <akladiost@gmail.com>
2022-08-17 17:58:09 +0200

    New version Théo

---
+++ SGA/Assets/script/Quitbutton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitbutton : MonoBehaviour
+{
+public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
